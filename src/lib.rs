9ad25cfb0d7555a20c7fd99d7c2a2c01e5163f9d
dhruvgupta3377 <dhruvgupta3377@gmail.com>
2024-04-25 20:49:18 +0530

    create todos done

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,10 @@ pub fn listgenerator(vec: Vec<Task>) -> String {
     // println!("{:?}", vec.get(1).unwrap()._id.to_string());
     let mut htmlcontent = String::new();
     for item in vec.iter() {
-        let temp = format!("<li>{}<t/><button>Done</button></li>", item.task);
+        let temp = format!("<li>{}<t/><button
+        hx-post=\"http://127.0.0.1:8000/delete/{}\"
+        hx-target=\"#result\"
+        hx-swap=\"innerHTML\" >Done</button></li>", item.task,item._id);
 
         htmlcontent.push_str(temp.as_str());
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use rocket::form::Form;
 use rocket::{futures::TryStreamExt , http::ContentType};
 use rust_htmx_todo::datastruct::Task;
-
+use serde::{Deserialize, Serialize};
+use rocket::serde::json::Json;
 
 use rust_htmx_todo::listgenerator;
 use std::{fs::File, vec};
@@ -8,18 +10,24 @@ use std::io::Read;
 
 
 use mongodb::{bson::doc, Client, Collection};
+// use mongodb::{ bson::doc, sync::{ Client, Collection } };
 
+#[derive(Serialize, Deserialize, Debug, FromForm)]
+struct TaskDoc{
+    task:String
+}
+
+
+
+static URL: &str = "mongodb+srv://dhruvgupta3377:fD4Sn5RSdFGRvzE4@todos.dviwdft.mongodb.net/?retryWrites=true&w=majority&appName=todos";
 
 async fn find_all() -> String{
-    let uri = "mongodb+srv://dhruvgupta3377:fD4Sn5RSdFGRvzE4@todos.dviwdft.mongodb.net/?retryWrites=true&w=majority&appName=todos";
-    let client = Client::with_uri_str(uri).await.unwrap();
+    let client = Client::with_uri_str(URL).await.unwrap();
     println!("doing some thing");
     let my_coll: Collection<Task> = client
         .database("todo-db")
         .collection("todo-collection");
-    let mut cursor = my_coll.find(doc! {}, None).await.unwrap();
-    
-
+    let mut cursor = my_coll.find(None, None).await.expect("fucked up here");
     let mut vec:Vec<Task> = Vec::new();
     while let Some(doc) = cursor.try_next().await.unwrap() {
         vec.push(doc);
@@ -35,15 +43,25 @@ extern crate rocket;
 fn home() -> (ContentType, String) {
     let file_path = "index.html";
     let mut file = File::open(file_path).expect("Can't open the file");
-
     let mut html_content = String::new();
     file.read_to_string(&mut html_content)
         .expect("Can't convert to string");
     (ContentType::HTML, html_content)
 }
 
-#[post("/")]
-async fn create() -> (ContentType, String) {
+#[post("/", data = "<task>")]
+async fn create(task:Form<TaskDoc>) -> (ContentType, String) {
+    let client = Client::with_uri_str(URL).await.unwrap();
+    println!("doing some thing");
+    let my_coll: Collection<TaskDoc> = client
+        .database("todo-db")
+        .collection("todo-collection");
+    if task.task.clone() != "".to_string(){
+        let doc = TaskDoc {
+            task: task.task.clone()
+        };
+        my_coll.insert_one(doc, None).await.expect("Error in creating Task");
+    }
     let s = find_all().await;
     (ContentType::HTML, s)
 }
@@ -64,11 +82,3 @@ async fn rocket() -> _ {
         .mount("/delete",routes![delete])
 }
 
-// TODO: Connect to a database
-// TODO: Learn Tokio
-// TODO: Make an rs library
-// TODO: Add new TODO functionality
-// TODO: Add delete functionality
-
-// fD4Sn5RSdFGRvzE4
-// dhruvgupta3377
